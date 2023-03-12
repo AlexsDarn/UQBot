aa36e689e19519fc9de525e928e0f11b0bb6a186
Sebastian Lugo <sebastian.lugom@uqvirtual.edu.co>
2023-03-12 14:24:31 -0500

    modification to main.py

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -35,7 +35,7 @@ labels = sorted(labels)
 training = []
 output = []
 
-out_empty = [0 for _ in range(len(classes))]
+out_empty = [0 for _ in range(len(labels))]
 
 for x, doc in enumerate(docs_x):
     bag = []
@@ -55,4 +55,17 @@ for x, doc in enumerate(docs_x):
     output.append(output_row)
 
 training = numpy.array(training)
-output = np.array(output)
+output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
