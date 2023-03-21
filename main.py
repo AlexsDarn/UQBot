1443f3ea947b1f094bc31202f0d58ac10ec2a512
AlexsDarn <alexisd4rk@gmail.com>
2023-03-21 15:10:07 -0500

    inicio de rama

--- main.py
+++ main.py
@@ -77,15 +77,7 @@ net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
-model = tflearn.DNN(net)
-
-#try:
-#    model.load('./model.tflearn')
-#except:
-#    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-#    model.save('./model.tflearn')
-    
-    
+model = tflearn.DNN(net)    
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("./model.tflearn")
 
